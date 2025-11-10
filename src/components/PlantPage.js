39b3173885736ca82667a11296f5ee3f2a28f5f7
Snyder-F8 <fridahnzomo857@gmail.com>
2025-11-10 12:08:43 +0300

    Changes in plantpage js file

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
-import NewPlantForm from "./NewPlantForm";
+import React, { useState } from "react";
 import PlantList from "./PlantList";
+import NewPlantForm from "./NewPlantForm";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ plants, setPlants }) {
+  const [search, setSearch] = useState("");
+
+  function handleAddPlant(plant) {
+    setPlants([...plants, plant]);
+  }
+
+  function handleUpdatePlant(updatedPlant) {
+    const updatedPlants = plants.map((p) =>
+      p.id === updatedPlant.id ? updatedPlant : p
+    );
+    setPlants(updatedPlants);
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search onSearchChange={setSearch} />
+      <PlantList plants={filteredPlants} onUpdatePlant={handleUpdatePlant} />
     </main>
   );
 }
