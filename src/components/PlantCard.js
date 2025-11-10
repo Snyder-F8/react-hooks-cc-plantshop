4b418b9b6657294bd0dd3b4fa5405a440962c724
Snyder-F8 <fridahnzomo857@gmail.com>
2025-11-10 11:51:12 +0300

    Changes in plantcard file

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,27 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onUpdatePlant }) {
+  function handleStockClick() {
+    const updated = { ...plant, soldOut: !plant.soldOut };
+    onUpdatePlant(updated);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: ${plant.price}</p>
+
+      {plant.soldOut ? (
+        <button onClick={handleStockClick}>Out of Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button className="primary" onClick={handleStockClick}>
+          In Stock
+        </button>
       )}
     </li>
   );
 }
 
 export default PlantCard;
+
